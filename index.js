00517de406270f6d44ecd5417910a4e8fe291075
mvoloskov <hello@miloslav.website>
2022-02-07 22:02:54 +0300

    redundant file removed

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-var eslintrc = require('./.eslintrc.js')
-module.exports = eslintrc
+module.exports = {
+	extends: [],
+	plugins: [],
+	rules: {
+		'for-direction': 'error',
+		'no-async-promise-executor': 'error',
+		'no-await-in-loop': 'error',
+		'no-compare-neg-zero': 'error',
+		'no-cond-assign': 'error',
+		'no-dupe-args': 'error',
+		'no-dupe-else-if': 'error',
+		'no-dupe-keys': 'error',
+		'no-duplicate-case': 'error',
+		'no-func-assign': 'error',
+		'no-import-assign': 'error',
+		'no-invalid-regexp': 'error',
+		'no-loss-of-precision': 'error',
+		'no-obj-calls': 'error',
+		'no-unsafe-negation': 'error',
+		'require-atomic-updates': 'error',
+		'use-isnan': 'error',
+		'valid-typeof': 'error',
+		'no-const-assign': 'error',
+		'no-dupe-class-members': 'error',
+		'no-duplicate-imports': 'error',
+		'no-new-symbol': 'error',
+		'no-this-before-super': 'error',
+	},
+}
